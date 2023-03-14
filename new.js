b5ab5fe106c90319067cb834d14bc8b8e781c940
Rehan-Shah12 <100332064+Rehan-Shah12@users.noreply.github.com>
2023-03-14 16:36:02 +0500

    ishopping added to the new.js

--- new.js
+++ new.js
@@ -11,13 +11,22 @@ app.use(bodyParser.json());
 
 app.post("/search", async (req, res) => {
   const query = req.body.query;
-  const [eloProducts, darazProducts] = await Promise.all([
+  const [eloProducts, darazProducts, ishoppingProducts] = await Promise.all([
     elo(query),
     daraz(query),
+    ishopping(query),
   ]);
-  const jsonData = JSON.stringify({ daraz: darazProducts, elo: eloProducts });
+  const jsonData = JSON.stringify({
+    daraz: darazProducts,
+    elo: eloProducts,
+    ishopping: ishoppingProducts,
+  });
   fs.writeFileSync("search_data.json", jsonData);
-  res.json({ daraz: darazProducts, elo: eloProducts });
+  res.json({
+    daraz: darazProducts,
+    elo: eloProducts,
+    ishopping: ishoppingProducts,
+  });
 });
 
 app.listen(port, () => {
@@ -54,9 +63,10 @@ async function elo(query) {
   await page.keyboard.press("Enter");
 
   await page.waitForNavigation();
-  await page.waitForSelector(
-    "#shopify-section-template--16146192761011__main > section > div.container.search__content > main > div.search__results-list > div"
-  );
+  // await page.waitForSelector(
+  //   "#shopify-section-template--16146192761011__main > section > div.container.search__content > main > div.search__results-list > div"
+  // );
+  await page.waitForSelector("div.search__item.container.has-padding-bottom");
   //   await page.waitForSelector("div.search__item.container.has-padding-bottom");
 
   const html = await page.content();
@@ -64,9 +74,7 @@ async function elo(query) {
 
   const products = [];
 
-  $(
-    "#shopify-section-template--16146192761011__main > section > div.container.search__content > main > div.search__results-list > div"
-  ).each((index, element) => {
+  $("div.search__item.container.has-padding-bottom").each((index, element) => {
     const name = $(element).find("h3.search-result__title > a").text();
     const price = $(element).find("span.price > span.money").text().trim();
     const orignalprice = $(element)
@@ -89,6 +97,9 @@ async function elo(query) {
 
   return products;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////
+
 async function daraz(query) {
   const browser = await puppeteer.launch({
     headless: false,
@@ -139,5 +150,62 @@ async function daraz(query) {
 
   await browser.close();
 
+  return products;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+async function ishopping(query) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    ignoreDefaultArgs: ["--disable-extentions"],
+    args: ["--no-sandbox"],
+  });
+
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+
+  // Overrides the hieghts property
+  const override = Object.assign(page.viewport(), { width: 1366 });
+  await page.setViewport(override);
+
+  // This gives an error for no height
+  // const width = 1024;
+  // await page.setViewport({ width: width });
+  await page.setUserAgent("UA-TEST");
+
+  const url = "https://www.ishopping.pk/";
+  await page.goto(url);
+
+  // waiting for search bar to load
+  await page.waitForSelector("input#search");
+  await page.type("input#search", query);
+  await page.keyboard.press("Enter");
+
+  await page.waitForNavigation();
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  await page.waitForSelector("li.col-xs-6.col-sm-4.col-md-3.item");
+
+  const products = [];
+
+  $("li.col-xs-6.col-sm-4.col-md-3.item ").each((index, element) => {
+    const name = $(element).find("h2.product-name > a").text();
+    const price = $(element)
+      .find("p.special-price > span.price, span.regular-price > span.price")
+      .text()
+      .trim();
+    const oldprice = $(element).find("p.old-price > span.price").text().trim();
+    const image = $(element)
+      .find("div.inner-grid > a.product-image > img")
+      .attr("src");
+    //   const desc = $(element).find("div.has-padding-top > p").text().trim();
+    const link = $(element).find("h2.product-name > a").attr("href");
+    const site = "Ishopping";
+    products.push({ name, price, oldprice, image, link, site });
+  });
+  //   console.log(JSON.stringify(products, null, 2));
+
+  await browser.close();
   return products;
 }
